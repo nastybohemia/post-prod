6ff7dcc3432707b29afefd1208a56afa8fc499b0
Nasty Bohemia <sophie@post-production.team>
2020-03-19 02:19:49 -0400

    arists search

---
+++ home.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>POST-PROD</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicons -->
+    <link rel="" href="" type="">
+
+    <!-- ************************* CSS Files ************************* -->
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <!-- Preloader Start -->
+    <div class="">
+    </div>
+    <!-- Preloader End -->
+    <div id="text-container">
+    <h1 class="red-text">
+    <span class="red">POST-PROD</span><span> is the first<br>
+post-production agency<br>
+in Los Angeles.</span>
+</h1>
+<h5 class="description">
+    <span>Our artists do:</span><span class="blue"> Video Editing | Sound Editing | VFX | Color Correction | 3D | Animation</span>
+</h5>
+</div>
+<div id="btn-container">
+<button class="btn-blue">SEARCH ARTISTS</button>
+<br><br><br>
+<form action="index.php" method="post">
+    <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Type</button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+            <label class="form-check-label" for="defaultCheck1">Video Editing</label><br>
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck2">
+        <label class="form-check-label" for="defaultCheck2">Audio Editing</label><br>
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck3">
+        <label class="form-check-label" for="defaultCheck3">Color Correction</label><br>
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck4">
+        <label class="form-check-label" for="defaultCheck4">VFX</label><br>
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck5">
+        <label class="form-check-label" for="defaultCheck5">3D</label><br>
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck6">
+        <label class="form-check-label" for="defaultCheck6">Animation</label><br>
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck7">
+        <label class="form-check-label" for="defaultCheck7">Motion Graphics</label>
+        </div></div></div>
+        <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Software</button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+        <label class="form-check-label" for="defaultCheck1">Adobe Premiere Pro</label><br>
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck2">
+        <label class="form-check-label" for="defaultCheck2">Adobe After Effects</label><br>
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck3">
+        <label class="form-check-label" for="defaultCheck3">DaVinci Resolve</label><br>
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck4">
+        <label class="form-check-label" for="defaultCheck4">Final Cut X</label><br>
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck5">
+        <label class="form-check-label" for="defaultCheck5">Avid</label><br>
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck6">
+        <label class="form-check-label" for="defaultCheck6">Adobe Audition</label><br>
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck7">
+        <label class="form-check-label" for="defaultCheck7">Pro Tools</label><br>
+        </div></div></div>
+        <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Language</button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+        <label class="form-check-label" for="defaultCheck1">English</label><br>
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck2">
+        <label class="form-check-label" for="defaultCheck2">Spanish</label><br>
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck3">
+        <label class="form-check-label" for="defaultCheck3">Chineese</label><br>
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck4">
+        <label class="form-check-label" for="defaultCheck4">Russian</label><br>
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck5">
+        <label class="form-check-label" for="defaultCheck5">Japaneese</label>
+        </div></div></div>
+</form>
+</div>
+    <!-- ************************* JS Files ************************* -->
+    <!-- jQuery JS -->
+    <script src="assets/js/vendor.js"></script>
+    <!-- Main JS -->
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
