2f45e2e8f4f878bb0cf30dd3bca73ccb6ca7b9fe
Nasty Bohemia <sophie@post-production.team>
2020-03-30 20:11:47 -0400

    faq page

---
+++ faq.php
@@ -0,0 +1,254 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>FAQ</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicons -->
+    <link rel="" href="" type="">
+    <!-- ************************* JQuery *************************-->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+    crossorigin="anonymous"></script>
+    <!-- ************************* Bootstrap *************************-->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <!-- ************************* CSS Files *************************-->
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/ehya.css">
+    <!-- ************************* Semantic *************************-->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
+    <!-- ************************* JS Code ************************* -->
+    </head>
+    <body>
+        <!--start:hero-->
+    <div id="hero" class="hero">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-12 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2">
+                    <div class="text-center">
+                        <h1 class="text-hkgrotesk font-bold text-alt text-white mb-40">We are here to help you</h1>
+                        <div class="input-group form-search mb-24">
+                            <div class="input-group-prepend">
+                                <i class="fas fa-search"></i>
+                            </div>
+                            <input type="text" class="rounded-8 form-control bg-white" aria-label="" placeholder="Ask a questions…">
+                            <div class="input-group-append">
+                                <button type="button" class="btn btn-primary btn-sm">Search</button>
+                            </div>
+                        </div>
+                        <p class="font-semibold text-white-600">*We are collect your searching keywords to improve our FAQ</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--end:hero-->
+</header>
+<!--end:header-->
+
+<!--start:section category-->
+<section id="category" class="category">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="heading text-center mb-52">
+                    <h5 class="text-opensans font-semibold text-gray mb-0">Or choose a category to quickly find the help you need</h5>
+                </div>
+            </div>
+        </div>
+        <div class="content">
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-4 col-xl-4">
+                    <div class="category-card rounded-8 border box-shadow py-40 px-32 text-center">
+                        <div class="ic mb-16">
+                            <img src="../assets/images/icons/opened-file.svg" alt="">
+                        </div>
+                        <h5 class="text-hkgrotesk font-bold text-black mb-12">Getting started</h5>
+                        <p class="text-gray mb-0">All you need from A to Z to getting started are available.</p>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-12 col-lg-4 col-xl-4">
+                    <div class="category-card rounded-8 border box-shadow py-40 px-32 text-center">
+                        <div class="ic mb-16">
+                            <img src="../assets/images/icons/money-donation.svg" alt="">
+                        </div>
+                        <h5 class="text-hkgrotesk font-bold text-black mb-12">Pricing plan</h5>
+                        <p class="text-gray mb-0">All you need from A to Z to getting started are available.</p>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-12 col-lg-4 col-xl-4">
+                    <div class="category-card rounded-8 border box-shadow py-40 px-32 text-center">
+                        <div class="ic mb-16">
+                            <img src="../assets/images/icons/like.svg" alt="">
+                        </div>
+                        <h5 class="text-hkgrotesk font-bold text-black mb-12">Sales questions</h5>
+                        <p class="text-gray mb-0">All you need from A to Z to getting started are available.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--end:section category-->
+
+<!--start:section faq-->
+<section id="faq" class="faq clip-eb">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="heading text-center xl-mb-64">
+                    <h2 class="text-hkgrotesk font-bold text-alt text-black mb-14">Pricing plan</h2>
+                    <p class="text-18 leading-32 text-gray">Frequently asked questions about Pricing Plan</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-lg-10 offset-lg-1 col-xl-10 offset-xl-1">
+                <div class="accordion xl-mb-144" id="accordionPricingFAQ">
+                    <div class="card">
+                        <div class="card-header" id="headingOne">
+                            <h2 class="mb-0">
+                                <button class="text-hkgrotesk font-bold" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    <span>How do I pay for the Essentials or Premium plan?</span>
+
+                                    <i class="fas fa-plus"></i>
+                                </button>
+                            </h2>
+                        </div>
+
+                        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionPricingFAQ">
+                            <div class="card-body text-opensans">
+                                <p>You can pay with a credit card or via net banking (if you’re in United States). We will renew your subscription automatically at the end of every billing cycle.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header" id="headingTwo">
+                            <h2 class="mb-0">
+                                <button class="collapsed text-hkgrotesk font-bold" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                    <span>Can I cancel my Essentials or Premium plan subscription at any time?</span>
+
+                                    <i class="fas fa-plus"></i>
+                                </button>
+                            </h2>
+                        </div>
+                        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionPricingFAQ">
+                            <div class="card-body text-opensans">
+                                <p>You can pay with a credit card or via net banking (if you’re in United States). We will renew your subscription automatically at the end of every billing cycle.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header" id="headingThree">
+                            <h2 class="mb-0">
+                                <button class="collapsed text-hkgrotesk font-bold" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                    <span>We need to add new users to our team. How will that be billed?</span>
+
+                                    <i class="fas fa-plus"></i>
+                                </button>
+                            </h2>
+                        </div>
+                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionPricingFAQ">
+                            <div class="card-body text-opensans">
+                                <p>You can pay with a credit card or via net banking (if you’re in United States). We will renew your subscription automatically at the end of every billing cycle.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header" id="headingFour">
+                            <h2 class="mb-0">
+                                <button class="collapsed text-hkgrotesk font-bold" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                    <span>We need to add new users to our team. How will that be billed?</span>
+
+                                    <i class="fas fa-plus"></i>
+                                </button>
+                            </h2>
+                        </div>
+                        <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionPricingFAQ">
+                            <div class="card-body text-opensans">
+                                <p>You can pay with a credit card or via net banking (if you’re in United States). We will renew your subscription automatically at the end of every billing cycle.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header" id="headingFive">
+                            <h2 class="mb-0">
+                                <button class="collapsed text-hkgrotesk font-bold" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                                    <span>My team wants to cancel its subscription. How do we do that? Can we get a refund?</span>
+
+                                    <i class="fas fa-plus"></i>
+                                </button>
+                            </h2>
+                        </div>
+                        <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordionPricingFAQ">
+                            <div class="card-body text-opensans">
+                                <p>You can pay with a credit card or via net banking (if you’re in United States). We will renew your subscription automatically at the end of every billing cycle.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header" id="headingSix">
+                            <h2 class="mb-0">
+                                <button class="collapsed text-hkgrotesk font-bold" type="button" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                                    <span>Do you offer discounts for non-profit organizations or educational institutions?</span>
+
+                                    <i class="fas fa-plus"></i>
+                                </button>
+                            </h2>
+                        </div>
+                        <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordionPricingFAQ">
+                            <div class="card-body text-opensans">
+                                <p>You can pay with a credit card or via net banking (if you’re in United States). We will renew your subscription automatically at the end of every billing cycle.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="faq-question mb-56">
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2">
+                    <div class="heading text-center xl-mb-64">
+                        <h2 class="text-hkgrotesk font-bold text-alt text-black xl-mb-16">Still have a questions?</h2>
+                        <p class="xl-text-18 text-gray">If you cannot find answer to your question in our FAQ, you can always contact us. We wil answer to you shortly!</p>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-12 col-lg-10 offset-lg-1 col-xl-10 offset-xl-1">
+                    <div class="content">
+                        <div class="row">
+                            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                                <div class="faq-question-card border box-shadow rounded-8 xl-p-40 bg-white text-center">
+                                    <div class="ic mb-24">
+                                        <img src="../assets/images/icons/phone.svg">
+                                    </div>
+                                    <h5 class="text-hkgrotesk font-bold text-black mb-6px">+1 (646) 786-5060</h5>
+                                    <p class="text-gray">We are always happy to help.</p>
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                                <div class="faq-question-card border box-shadow rounded-8 xl-p-40 bg-white text-center">
+                                    <div class="ic mb-24">
+                                        <img src="../assets/images/icons/cs.svg" alt="">
+                                    </div>
+                                    <h5 class="text-hkgrotesk font-bold text-black mb-6px">support@helpcenter.com</h5>
+                                    <p class="text-gray">Alternative way to get anwser faster.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--end:section faq-->
+</body>
+</html>
+</body>
+</html>
+<?php 
+require 'footer.php';
+?>
