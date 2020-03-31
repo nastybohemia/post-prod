21eb99951b254aa54328cfd5496136090faf1802
Nasty Bohemia <sophie@post-production.team>
2020-03-30 20:14:47 -0400

    about us page

---
+++ about.php
@@ -0,0 +1,237 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>FAQ</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicons -->
+    <link rel="" href="" type="">
+    <!-- ************************* JQuery *************************-->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+    crossorigin="anonymous"></script>
+    <!-- ************************* Bootstrap *************************-->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <!-- ************************* CSS Files *************************-->
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/ehya.css">
+    <!-- ************************* Semantic *************************-->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
+    <!-- ************************* JS Code ************************* -->
+    </head>
+    <body>
+        <!--start:hero-->
+    <div id="hero" class="hero">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-12 col-lg-8 offset-lg-2">
+                    <div class="hero-content text-center px-52">
+                        <h1 class="text-hkgrotesk font-bold text-alt text-white mb-40">Experience you can trust</h1>
+                        <p class="text-20 leading-36 text-white mb-40">From year to year we strive to invent the most innovative technology that is used by both small enterprises and space enterprises.</p>
+                        <button type="button" class="btn btn-lg btn-white btn-rounded text-primary">Read our story</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--end:hero-->
+</header>
+<!--end:header-->
+
+<!--start:about-->
+<section id="about" class="about xl-pt-64">
+    <div class="container">
+        <div class="heading xl-mb-140">
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                    <div class="xl-pr-80">
+                        <span class="text-opensans font-semibold text-14 tracking-2 text-primary">ABOUT US</span>
+                        <h2 class="text-hkgrotesk font-bold text-alt text-black mt-16">We provide enterprises
+                            with innovative technology
+                            for small to space enterprises</h2>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                    <div class="pt-24 xl-pt-36">
+                        <p class="text-18 leading-32 text-black xl-mb-32">We supply enterprises, organizations and institutes of
+                            high-tech industries with modern components. We build long-term trusting relationships with our customers and partnes for further fruitful cooperations.</p>
+
+                        <p class="text-18 leading-32 text-black">From year to year we strive to invent the most innovative technology that is used by both small enterprises and space enterprises.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="content">
+        <ul class="list-unstyled d-flex flex-wrap">
+            <li class="w-380 h-460 mr-24 mb-24">
+                <img src="../assets/images/photos/380x460/1@2x.png" alt="" class="rounded-8 object-cover w-100 h-100">
+            </li>
+            <li class="w-360 h-240 mr-24 mb-24 position-relative mt-80">
+                <img src="../assets/images/photos/360x240/1@2x.png" alt="" class="rounded-8 object-cover w-100 h-100">
+            </li>
+            <li class="w-440 h-320 mr-24 mb-24 position-absolute right-0 -mr-24">
+                <img src="../assets/images/photos/440x320/1@2x.png" alt="" class="rounded-8 object-cover w-100 h-100">
+            </li>
+            <li class="w-440 h-320 mr-24 position-relative -ml-76 mr-56">
+                <img src="../assets/images/photos/440x320/2@2x.png" alt="" class="rounded-8 object-cover w-100 h-100">
+            </li>
+            <li class="w-210 h-240 mr-24 -ml-32">
+                <img src="../assets/images/photos/210x240/1@2x.png" alt="" class="rounded-8 object-cover w-100 h-100">
+            </li>
+            <li class="w-600 h-420 mr-24 position-relative -mt-140">
+                <img src="../assets/images/photos/600x420/1@2x.png" alt="" class="rounded-8 object-cover w-100 h-100">
+            </li>
+            <li class="w-120 position-relative -mt-140">
+                <img src="../assets/images/pattern/about-pattern.svg" alt="">
+            </li>
+        </ul>
+    </div>
+</section>
+<!--end:about-->
+
+<!--start:values-->
+<section id="values" class="values xl-pt-200 xl-pb-120">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-lg-10 offset-lg-1 col-xl-10 offset-xl-1">
+                <div class="heading text-center mb-100">
+                    <h2 class="text-alt text-black xl-mb-18">Our values</h2>
+                    <p class="xl-text-18 text-gray mb-0">We strive to redefine the standard of excellence.</p>
+                </div>
+
+                <div class="content">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                            <div class="values-card text-center d-flex flex-column justify-center items-center px-32 xl-mb-48">
+                                <div class="ic ic-lg ic-badge bg-primary-100 mb-32">
+                                    <img src="../assets/images/icons/application.svg" alt="">
+                                </div>
+                                <h5 class="text-black mb-16">Collaboration</h5>
+                                <p class="text-gray mb-0">Collaboration is the process of two or more people or organizations working together to complete a task or achieve a goal. </p>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                            <div class="values-card text-center d-flex flex-column justify-center items-center px-32 xl-mb-48">
+                                <div class="ic ic-lg ic-badge bg-primary-100 mb-32">
+                                    <img src="../assets/images/icons/multiple-shape.svg" alt="">
+                                </div>
+                                <h5 class="text-black mb-16">Transparency</h5>
+                                <p class="text-gray mb-0">Transparency, as used in science is operating in such a way that it is easy for others to see what actions are performed.</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                            <div class="values-card text-center d-flex flex-column justify-center items-center px-32">
+                                <div class="ic ic-lg ic-badge bg-primary-100 mb-32">
+                                    <img src="../assets/images/icons/like.svg" alt="">
+                                </div>
+                                <h5 class="text-black mb-16">Trust</h5>
+                                <p class="text-gray mb-0">Trust will help us foster a positive and productive environment that delivers value to our users and customers.</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                            <div class="values-card text-center d-flex flex-column justify-center items-center px-32">
+                                <div class="ic ic-lg ic-badge bg-primary-100 mb-32">
+                                    <img src="../assets/images/icons/substract.svg" alt="">
+                                </div>
+                                <h5 class="text-black mb-16">Integrity</h5>
+                                <p class="text-gray mb-0">Integrity is the practice of showing a consistent and uncompromising adherence to strong moral and ethical principles.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--end:values-->
+
+<!--start:team-->
+<section id="team" class="team clip-eb -mb-140">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                <div class="heading pr-120">
+                    <span class="text-opensans font-semibold text-14 tracking-2 text-success">OUR TEAM</span>
+                    <h2 class="text-alt text-black mt-16 mb-24">Experience and integrity
+                        by our team</h2>
+                    <p class="text-18 text-gray mb-40">The right tools wielded by the right people to make anything possible. From year to year we strive to invent the most innovative technology produced by our creative people</p>
+                    <button type="button" class="btn btn-outline-primary btn-rounded">Meet our team <i class="fas fa-arrow-right text-14 ml-8"></i></button>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                <ul class="team-list list-unstyled d-flex flex-wrap">
+                    <li class="w-96 h-96 mr-64">
+                        <img src="../assets/images/photos/96x96/1@2x.png" alt="" class="w-100 object-cover rounded-12">
+                    </li>
+                    <li class="w-64 h-64 mr-48 position-relative mt-96">
+                        <img src="../assets/images/photos/64x64/1@2x.png" alt="" class="w-100 object-cover rounded-12">
+                    </li>
+                    <li class="w-160 h-180 position-relative -mt-112">
+                        <img src="../assets/images/photos/160x180/1@2x.png" alt="" class="w-100 object-cover rounded-12">
+                    </li>
+                    <li class="w-120 h-120 mr-104 position-relative ml-80 mt-56">
+                        <img src="../assets/images/photos/120x120/1@2x.png" alt="" class="w-100 object-cover rounded-12">
+                    </li>
+                    <li class="team-pattern">
+                        <img src="../assets/images/pattern/about-pattern.svg" alt="">
+                    </li>
+                    <li class="w-96 h-96 position-relative -mt-32">
+                        <img src="../assets/images/photos/96x96/2@2x.png" alt="" class="w-100 object-cover rounded-12">
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<!--end:team-->
+
+<!--start:customers-->
+<section id="customers" class="customers pt-200 pb-144">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-lg-7 col-xl-7">
+                <div class="heading mb-64 text-black">
+                    <h2 class="text-alt">Through the years we helped a lot of companies. Are you ready to become our partner?</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="content">
+                    <ul class="list-unstyled d-flex align-items-center">
+                        <li class="xl-mr-56">
+                            <img src="../assets/images/clients-logo/netflix.svg" alt="" class="h-32">
+                        </li>
+                        <li class="xl-mr-56">
+                            <img src="../assets/images/clients-logo/airbnb.svg" alt="" class="h-32">
+                        </li>
+                        <li class="xl-mr-56">
+                            <img src="../assets/images/clients-logo/fitbit.svg" alt="" class="h-32">
+                        </li>
+                        <li class="xl-mr-56">
+                            <img src="../assets/images/clients-logo/google.svg" alt="" class="h-32">
+                        </li>
+                        <li class="xl-mr-56">
+                            <img src="../assets/images/clients-logo/uber.svg" alt="" class="h-32">
+                        </li>
+                        <li>
+                            <button type="button" class="btn btn-lg btn-primary btn-rounded">Get a free quote <i class="fas fa-arrow-right text-14 ml-8"></i></button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--end:customers-->
+</body>
+</html>
+</body>
+</html>
+<?php 
+require 'footer.php';
+?>
