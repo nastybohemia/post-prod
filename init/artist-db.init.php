89ffa38f672b755fc9cc9ced03d3d981c402461f
Sophie <57526919+nastybohemia@users.noreply.github.com>
2020-07-20 14:49:53 -0700

    Delete artist-db.init.php

--- init/artist-db.init.php
+++
@@ -1,9 +0,0 @@
-<?php
-mysql_connect("localhost:3306","root","password") or die("could not connect");
-mysql_select_db("artists") or die("could not find db");
-//collect
-if (isset($_POST['artist-search'])){
-    $searchq = $_POST['artist-search'];
-    $query = mysql_query("SELECT * FROM ") or die("could not search");
-}
-?>
